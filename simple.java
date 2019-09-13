47e035dffc24f7d8d71a915233ba2fc0fa14ee3f
shindera <shinderaj526@gmail.com>
2019-09-13 14:40:57 +0530

    testing

--- simple.java
+++ simple.java
@@ -2,5 +2,6 @@ public class simple{
     public static void main(String args[]){  
      System.out.println("Hello World");  
       System.out.println("Hello Universe");
+      System.out.println("Hello Wakaanda");
     }  
 }  
