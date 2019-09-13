2fddf2da9c1111978e3d805d912ae0177acd3e65
shindera <shinderaj526@gmail.com>
2019-09-13 14:58:52 +0530

    testing

--- simple.java
+++ simple.java
@@ -3,6 +3,6 @@ public class simple{
      System.out.println("Hello World");  
       System.out.println("Hello Universe");
       System.out.println("Hello Wakaanda");
-      System.out.println("Hello");
+      System.out.println("Hello_dev");
     }  
 }  
